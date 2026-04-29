9cf59173d2e05e33aee1121b15a991defd54e260
Mpho <matebesimoe89@gmail.com>
2026-04-29 17:56:45 +0200

    answer for ID 05 reducing depreciation

--- scripts_sql/answers/answers.sql
+++ scripts_sql/answers/answers.sql
@@ -23,8 +23,13 @@
    FROM [financial_engineering_db].[dbo].[financial_tasks]
    WHERE Task_ID = '4'
 
-   /* 5.  Reducing Balance Depreciation: A = P * (1 - i)^n
-   6.  Quarterly Compound: A = P * (1 + r/4)^(4 * n)
+   /* 5.  Reducing Balance Depreciation: A = P * (1 - i)^n */
+    SELECT 
+		CAST((Principal * POWER( 1 - Annual_Rate, Term_Years)) AS DECIMAL (10,2))  reducing_depreciation
+   FROM [financial_engineering_db].[dbo].[financial_tasks]
+   WHERE Task_ID = '5'
+
+   /* 6.  Quarterly Compound: A = P * (1 + r/4)^(4 * n)
    7.  Monthly Loan Accrual: I = [P * (1 + r/12)^12] - P
    8.  Doubling Time (Simple): t = 1 / r
    9.  Effective Annual Rate: EAR = (1 + r/m)^m - 1
