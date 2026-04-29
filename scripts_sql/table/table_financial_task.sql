a34aaa87203fbb1c43c22ebf6628a1b64ad4064e
Mpho <matebesimoe89@gmail.com>
2026-04-29 16:24:22 +0200

    insert values into the created financial tasks table

--- scripts_sql/table/table_financial_task.sql
+++ scripts_sql/table/table_financial_task.sql
@@ -14,3 +14,17 @@ CREATE TABLE dbo.financial_tasks (
 GO
 
 --Insert column names and their values into the table
+INSERT INTO dbo.financial_tasks 
+    (Task_ID, Model_Name, Principal, Annual_Rate, Term_Years, Frequency)
+VALUES 
+    (1, 'Simple Interest', 4500.00, 0.07, 5, 1),
+    (2, 'Compound Interest', 12000.00, 0.065, 8, 1),
+    (3, 'Hire Purchase', 18700.00, 0.11, 3, 1),
+    (4, 'Inflation Analysis', 1550.00, 0.055, 12, 1),
+    (5, 'Reducing Balance Depreciation', 480000.00, 0.18, 6, 1),
+    (6, 'Quarterly Compounding', 95000.00, 0.09, 4, 4),
+    (7, 'Monthly Loan Accrual', 30000.00, 0.14, 1, 12),
+    (8, 'Capital Doubling Time', 15000.00, 0.125, 0, 1),
+    (9, 'Effective Annual Rate', 1.00, 0.132, 1, 12),
+    (10, 'Semi-Annual Growth', 2500000.00, 0.15, 10, 2);
+GO
