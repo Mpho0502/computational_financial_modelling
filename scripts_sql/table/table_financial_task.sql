b5c971e8dbedeb5a32f9d06f4202c0f67ffbd78f
Mpho <matebesimoe89@gmail.com>
2026-04-29 16:17:22 +0200

    created table folder and insert table script

---
+++ scripts_sql/table/table_financial_task.sql
@@ -0,0 +1,13 @@
+IF OBJECT_ID('dbo.financial_tasks', 'U') IS NOT NULL
+    DROP TABLE dbo.financial_tasks;
+GO
+
+CREATE TABLE dbo.financial_tasks (
+    Task_ID      INT PRIMARY KEY,
+    Model_Name   VARCHAR(100) NOT NULL,
+    Principal   DECIMAL(19, 4),   
+    Annual_Rate  DECIMAL(19, 4),   
+    Term_Years   INT,
+    Frequency   INT DEFAULT 1     
+);
+GO
