240e123f8ad0a57cfd73a0dd1436df7abe04a2a1
Mpho <matebesimoe89@gmail.com>
2026-04-29 16:21:42 +0200

    comment a heading instruction for database script

--- scripts_sql/database/create_financial_eng_database.sql
+++ scripts_sql/database/create_financial_eng_database.sql
@@ -1,3 +1,4 @@
+--Create a financial engineering database if it does not exist
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'financial_engineering_db')
 BEGIN
     CREATE DATABASE financial_engineering_db;
